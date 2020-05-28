c76e7b1677109a4f25c379486fa16435c84cb0a1
Crowdbotics <team@crowdbotics.com>
2020-05-28 03:59:37 +0000

    Update models

--- backend/itsnotonlyadollar_17492/wsgi.py
+++ backend/itsnotonlyadollar_17492/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'itsnotonlyadollar_17492.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "itsnotonlyadollar_17492.settings")
 
 application = get_wsgi_application()
